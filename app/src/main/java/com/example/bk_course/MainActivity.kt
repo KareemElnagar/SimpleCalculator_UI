bc6910ddb0dffecf41e4670d9336054c615dc8c0
Kareem Elnagar <kareemelnagar733@gmail.com>
2023-10-15 00:58:57 +0200

    Updating code and icon launcher

--- app/src/main/java/com/example/bk_course/MainActivity.kt
+++ app/src/main/java/com/example/bk_course/MainActivity.kt
@@ -1,62 +1,54 @@
 package com.example.bk_course
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bk_course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var plusIcon: Button
-    lateinit var minusIcon: Button
-    lateinit var divIcon: Button
-    lateinit var multiplyIcon: Button
-    lateinit var resultIcon: Button
-    lateinit var textNumber: TextView
-    lateinit var clearIcon: Button
-    lateinit var modulesIcon: Button
-
-
     var historyNum: Double = 0.0
     var currentOperation: Operation? = null
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initView()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         addCallbacks()
 
     }
 
     fun addCallbacks() {
-        clearIcon.setOnClickListener {
+        binding.clearIcon.setOnClickListener {
             clearAll()
         }
-        plusIcon.setOnClickListener {
+        binding.plusIcon.setOnClickListener {
             prepareOperation(Operation.plus)
 
         }
-        minusIcon.setOnClickListener {
+        binding.minusIcon.setOnClickListener {
             prepareOperation(Operation.minus)
         }
-        multiplyIcon.setOnClickListener {
+        binding.multiplyIcon.setOnClickListener {
             prepareOperation(Operation.multiply)
         }
-        divIcon.setOnClickListener {
+        binding.divideIcon.setOnClickListener {
             prepareOperation(Operation.divide)
         }
-        modulesIcon.setOnClickListener {
+        binding.modules.setOnClickListener {
             prepareOperation(Operation.modules)
         }
 
-        resultIcon.setOnClickListener {
+        binding.equalIcon.setOnClickListener {
             val result = doCurrentOperation()
-            textNumber.text = result.toString()
+            binding.textNum.text = result.toString()
         }
     }
 
     private fun doCurrentOperation(): Double {
-        val currentNum = textNumber.text.toString().toDouble()
+        val currentNum = binding.textNum.text.toString().toDouble()
         return when (currentOperation) {
             Operation.plus -> historyNum + currentNum
             Operation.minus -> historyNum - currentNum
@@ -67,39 +59,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initView() {
-        plusIcon = findViewById(R.id.plusIcon)
-        minusIcon = findViewById(R.id.minusIcon)
-        divIcon = findViewById(R.id.divideIcon)
-        multiplyIcon = findViewById(R.id.multiplyIcon)
-        resultIcon = findViewById(R.id.equalIcon)
-        textNumber = findViewById(R.id.text_num)
-        clearIcon = findViewById(R.id.clearIcon)
-        modulesIcon = findViewById(R.id.modules)
-    }
-
-
     private fun prepareOperation(operation: Operation) {
-        historyNum = textNumber.text.toString().toDouble()
+        historyNum = binding.textNum.text.toString().toDouble()
         clearText()
         currentOperation = operation
     }
 
 
     private fun clearText() {
-        textNumber.text = ""
+        binding.textNum.text = ""
     }
 
     private fun clearAll() {
-        textNumber.text = ""
+        binding.textNum.text = ""
         historyNum = 0.0
     }
 
     fun onClickNum(v: View) {
         val newDigit = (v as Button).text.toString()
-        val oldDigit = textNumber.text.toString()
+        val oldDigit = binding.textNum.text.toString()
         val newTextNum = oldDigit + newDigit
-        textNumber.text = newTextNum
+        binding.textNum.text = newTextNum
 
 
     }
